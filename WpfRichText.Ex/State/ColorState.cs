9b2f5856c1cb3e1eb79c0348983f4b67c1f11e41
Skipidon <mr.skip995@mail.ru>
2015-11-05 00:06:09 +0200

    Minor changes

---
+++ WpfRichText.Ex/State/ColorState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace WpfRichText.Ex.State
+{
+    class ColorState : State
+    {
+        public ColorState()
+        {
+            StateColor = Colors.Black;
+        }
+    }
+}
---
+++ WpfRichText.Ex/State/IState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace WpfRichText.Ex.State
+{
+    public interface IState
+    {
+        Color StateColor { get; set; }
+    }
+}
