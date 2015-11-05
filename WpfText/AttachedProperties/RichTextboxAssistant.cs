dd71c7d6d2c7eeb3b7c8401eda02214716bd3496
Skipidon <mr.skip995@mail.ru>
2015-11-05 11:13:43 +0200

    new path

---
+++ WpfText/AttachedProperties/RichTextboxAssistant.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Data;
+
+namespace WpfRichText.Ex.AttachedProperties
+{
+    public static class RichTextboxAssistant
+    {
+        public static readonly DependencyProperty BoundDocument =
+           DependencyProperty.RegisterAttached("BoundDocument", typeof(string), typeof(RichTextboxAssistant),
+           new FrameworkPropertyMetadata(null,
+               FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+               OnBoundDocumentChanged)
+               );
+
+        private static void OnBoundDocumentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RichTextBox box = d as RichTextBox;
+
+            if (box == null)
+                return;
+
+            RemoveEventHandler(box);
+
+        
+
+        }
+
+        private static void RemoveEventHandler(RichTextBox box)
+        {
+            Binding binding = BindingOperations.GetBinding(box, BoundDocument);
+
+            if (binding != null)
+            {
+                if (binding.UpdateSourceTrigger == UpdateSourceTrigger.Default ||
+                    binding.UpdateSourceTrigger == UpdateSourceTrigger.LostFocus)
+                {
+
+                    box.LostFocus -= HandleLostFocus;
+                }
+                else
+                {
+                    box.TextChanged -= HandleTextChanged;
+                }
+            }
+        }
+
+        private static void AttachEventHandler(RichTextBox box)
+        {
+            Binding binding = BindingOperations.GetBinding(box, BoundDocument);
+
+            if (binding != null)
+            {
+                if (binding.UpdateSourceTrigger == UpdateSourceTrigger.Default ||
+                    binding.UpdateSourceTrigger == UpdateSourceTrigger.LostFocus)
+                {
+
+                    box.LostFocus += HandleLostFocus;
+                }
+                else
+                {
+                    box.TextChanged += HandleTextChanged;
+                }
+            }
+        }
+
+        private static void HandleLostFocus(object sender, RoutedEventArgs e)
+        {
+            RichTextBox box = sender as RichTextBox;
+
+            TextRange tr = new TextRange(box.Document.ContentStart, box.Document.ContentEnd);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                tr.Save(ms, DataFormats.Xaml);
+                string xamlText = ASCIIEncoding.Default.GetString(ms.ToArray());
+                SetBoundDocument(box, xamlText);
+            }
+        }
+
+        private static void HandleTextChanged(object sender, RoutedEventArgs e)
+        {
+            // TODO: TextChanged is currently not working!
+            RichTextBox box = sender as RichTextBox;
+
+            TextRange tr = new TextRange(box.Document.ContentStart,
+                               box.Document.ContentEnd);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                tr.Save(ms, DataFormats.Xaml);
+                string xamlText = ASCIIEncoding.Default.GetString(ms.ToArray());
+                SetBoundDocument(box, xamlText);
+            }
+        }
+
+        
+
+        public static void SetBoundDocument(DependencyObject dp, string value)
+        {
+            var xaml = value;
+            
+        }
+    }
+
+ 
+}
---
+++ WpfText/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Text_Editor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FIOT")]
+[assembly: AssemblyProduct("Text_Editor")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Zagorulko Andrey")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
